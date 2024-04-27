e3ea18b0c5622a7f657a9af1bc1d2f659f51224f
jessecarvalho <jesseelias80@gmail.com>
2024-04-27 18:17:37 -0300

    [feat] Added data annotations for clients

--- Src/Core/Entities/Client.cs
+++ Src/Core/Entities/Client.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 
 namespace Core.Entities;
@@ -5,7 +6,16 @@ namespace Core.Entities;
 public record Client
 {
     public BigInteger Id { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
     public string Name { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
     public string TelephoneNumber { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
     public List<Appointment> Appointments { get; set; }
 }
