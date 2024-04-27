6115c083eca5f95f87f2b01cbfdc3c6a66206b07
jessecarvalho <jesseelias80@gmail.com>
2024-04-27 18:18:04 -0300

    [feat] Added data annotations for services

--- Src/Core/Entities/Service.cs
+++ Src/Core/Entities/Service.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Numerics;
 using Core.Enums;
 
@@ -6,10 +8,25 @@ namespace Core.Entities;
 public record Service
 {
     public BigInteger Id { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
     public string Name { get; set; }
+    
+    [MaxLength(255)]
     public string Description { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
     public string Icon { get; set; }
-    public uint Price { get; set; }
+    
+    [Required]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+    
+    [Required]
     public uint TimeInMinutes { get; set; }
+    
+    [Required]
     public ActiveEnum Active { get; set; }
 }
