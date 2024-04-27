67b740dfda421330fff5e79e3c788a462bf10188
jessecarvalho <jesseelias80@gmail.com>
2024-04-27 18:22:37 -0300

    [feat] Added dependency injection for services and repositories

--- Src/Api/Configurations/DependencyInjectionConfig.cs
+++ Src/Api/Configurations/DependencyInjectionConfig.cs
@@ -1,7 +1,11 @@
+using Application.Interfaces;
+using Application.Services;
+using Infrastructure.Interfaces;
 using Infrastructure.Persistence;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-namespace Api.DependencyInjectionConfig;
+namespace Api.Configurations;
 
 public static class DependencyInjectionConfig
 {
@@ -9,6 +13,13 @@ public static class DependencyInjectionConfig
     {
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-        
+        services.AddScoped<IAppointmentService, AppointmentService>();
+        services.AddScoped<IClientService, ClientService>();
+        services.AddScoped<IEstablishmentService, EstablishmentService>();
+        services.AddScoped<IServiceService, ServiceService>();
+        services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+        services.AddScoped<IClientRepository, ClientRepository>();
+        services.AddScoped<IEstablishmentRepository, EstablishmentRepository>();
+        services.AddScoped<IServiceRepository, ServiceRepository>();
     }
 }
