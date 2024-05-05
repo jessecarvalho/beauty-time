6ee27a18aff77484c3620aa40e3ae94ee4c8b92e
jessecarvalho <jesseelias80@gmail.com>
2024-05-05 11:16:18 -0300

    [feat] Created relationship between entities

--- Src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ Src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -10,7 +10,26 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Appointment> Appointments { get; set; }
-    public DbSet<Establishment> BarberShops { get; set; }
+    public DbSet<Establishment> Establishments { get; set; }
     public DbSet<Client> Clients { get; set; }
     public DbSet<Service> Services { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>()
+            .HasMany(c => c.Appointments)
+            .WithOne(a => a.Client)
+            .HasForeignKey(a => a.ClientId);
+        modelBuilder.Entity<Client>()
+            .HasMany(c => c.Establishments)
+            .WithMany(e => e.Clients);
+        modelBuilder.Entity<Establishment>()
+            .HasMany(e => e.Appointments)
+            .WithOne(a => a.Establishment)
+            .HasForeignKey(a => a.EstablishmentId);
+        modelBuilder.Entity<Establishment>()
+            .HasMany(e => e.Services)
+            .WithOne(s => s.Establishment)
+            .HasForeignKey(s => s.EstablishmentId);
+    }
 }
