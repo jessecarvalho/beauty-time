3c084e374a4e4453f71c33b89505fb214a9032c2
jessecarvalho <jesseelias80@gmail.com>
2024-05-05 11:24:25 -0300

    [feat] Created script to run the migrations when application is started

--- Src/Api/Program.cs
+++ Src/Api/Program.cs
@@ -2,6 +2,8 @@ using Api.Configurations;
 using Api.Middleware;
 using Application.Mappings;
 using FluentValidation.AspNetCore;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +13,24 @@ builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddAutoMapper(typeof(ApplicationProfile));
 DependencyInjectionConfig.Configure(builder.Services, builder.Configuration);
+JwtConfig.Configure(builder.Services, builder.Configuration);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -24,6 +42,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.MapControllers();
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.Run();
