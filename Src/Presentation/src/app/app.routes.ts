6fb620af5babcd77f8d3772e18f27b0fc9a561c3
jessecarvalho <jesseelias80@gmail.com>
2024-05-22 21:05:40 -0300

    [feat] Created routes for establishment intern pages

--- Src/Presentation/src/app/app.routes.ts
+++ Src/Presentation/src/app/app.routes.ts
@@ -8,17 +8,25 @@ import {LoginComponent} from "./pages/establishmentSide/auth/pages/login/login.c
 import {FirstStepComponent} from "./pages/establishmentSide/auth/pages/register/multi-step/first-step/first-step.component";
 import {SecondStepComponent} from "./pages/establishmentSide/auth/pages/register/multi-step/second-step/second-step.component";
 import {ThirdStepComponent} from "./pages/establishmentSide/auth/pages/register/multi-step/third-step/third-step.component";
+import {CalendarComponent} from "./pages/establishmentSide/establishment/pages/calendar/calendar.component";
+import {ClientsComponent} from "./pages/establishmentSide/establishment/pages/clients/clients.component";
 
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'establishment',
     children: [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: FirstStepComponent },
-    { path: 'register/second-step', component: SecondStepComponent },
-    { path: 'register/third-step', component: ThirdStepComponent },
-    { path: '' , component: HomeEstablishmentComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: FirstStepComponent },
+      { path: 'register/second-step', component: SecondStepComponent },
+      { path: 'register/third-step', component: ThirdStepComponent },
+      { path: '' , component: HomeEstablishmentComponent },
+      { path: 'business', component: HomeEstablishmentComponent},
+      { path: 'appointments', component: HomeEstablishmentComponent},
+      { path: 'services' , component: ServiceComponent },
+      { path: 'services/:serviceId' , component: ServiceComponent },
+      { path: 'calendar' , component: CalendarComponent },
+      { path: 'clients', component: ClientsComponent}
 
   ]},
   {
