102ceedc51e18a25c40cf5b1231c49e81f0d2cbd
jessecarvalho <jesseelias80@gmail.com>
2024-05-22 21:13:38 -0300

    [feat] Added dependencies for redis

--- Src/Api/Configurations/DependencyInjectionConfig.cs
+++ Src/Api/Configurations/DependencyInjectionConfig.cs
@@ -1,9 +1,12 @@
 using Application.Interfaces;
 using Application.Services;
+using Core.Interfaces;
 using Infrastructure.Interfaces;
 using Infrastructure.Persistence;
+using Infrastructure.Redis;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace Api.Configurations;
 
@@ -21,5 +24,16 @@ public static class DependencyInjectionConfig
         services.AddScoped<IClientRepository, ClientRepository>();
         services.AddScoped<IEstablishmentRepository, EstablishmentRepository>();
         services.AddScoped<IServiceRepository, ServiceRepository>();
+        services.AddScoped<IRedisService, RedisService>();
+        
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var configurationOptions = ConfigurationOptions.Parse(configuration["Redis:ConnectionString"], true);
+            configurationOptions.AbortOnConnectFail = false;
+            return ConnectionMultiplexer.Connect(configurationOptions);
+        });
+        
+        services.AddScoped<IRedisService, RedisService>();
+        
     }
 }
