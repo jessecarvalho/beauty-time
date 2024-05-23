b2c49b466e11d47953b0f112c0b71bd63e1724b9
jessecarvalho <jesseelias80@gmail.com>
2024-05-22 21:06:38 -0300

    [feat] Added mapper for serviceRequestDto to Service

--- Src/Application/Mappings/ApplicationProfile.cs
+++ Src/Application/Mappings/ApplicationProfile.cs
@@ -9,13 +9,14 @@ public class ApplicationProfile : Profile
 
     public ApplicationProfile()
     {
-        CreateMap<AppointmentRequestDto, Appointment>();
         CreateMap<Appointment, AppointmentResponseDto>();
-        CreateMap<EstablishmentRequestDto, Establishment>();
+        CreateMap<AppointmentRequestDto, Appointment>();
         CreateMap<Establishment, EstablishmentResponseDto>();
+        CreateMap<EstablishmentRequestDto, Establishment>();
         CreateMap<Client, ClientResponseDto>();
         CreateMap<ClientRequestDto, Client>();
         CreateMap<Service, ServiceResponseDto>();
+        CreateMap<ServiceRequestDto, Service>();
     }
     
 }
