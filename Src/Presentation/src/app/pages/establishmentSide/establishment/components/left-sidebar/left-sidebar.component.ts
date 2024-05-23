eba5275371207da04d03e9568414b88fdfda6810
jessecarvalho <jesseelias80@gmail.com>
2024-05-22 21:14:51 -0300

    [feat] Created links in sidebar

--- Src/Presentation/src/app/pages/establishmentSide/establishment/components/left-sidebar/left-sidebar.component.ts
+++ Src/Presentation/src/app/pages/establishmentSide/establishment/components/left-sidebar/left-sidebar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.scss'
 })
