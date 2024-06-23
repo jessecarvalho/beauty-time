e16cf0d319a0a7cc4e01fdd33031bb046514df3a
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 13:12:21 -0300

    [feat] Created migration for new user schema

---
+++ Src/Infrastructure/Persistence/Migrations/20240622150843_ChangeInUserMigration.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeInUserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Clients_ClientId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Establishments_EstablishmentId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_ClientId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_EstablishmentId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EstablishmentId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Establishments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Clients",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Establishments_UserId",
+                table: "Establishments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_UserId",
+                table: "Clients",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Users_UserId",
+                table: "Clients",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Establishments_Users_UserId",
+                table: "Establishments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Users_UserId",
+                table: "Clients");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Establishments_Users_UserId",
+                table: "Establishments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Establishments_UserId",
+                table: "Establishments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_UserId",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Establishments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Clients");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EstablishmentId",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ClientId",
+                table: "Users",
+                column: "ClientId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_EstablishmentId",
+                table: "Users",
+                column: "EstablishmentId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Clients_ClientId",
+                table: "Users",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Establishments_EstablishmentId",
+                table: "Users",
+                column: "EstablishmentId",
+                principalTable: "Establishments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
