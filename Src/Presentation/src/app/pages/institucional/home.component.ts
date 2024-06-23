ddaeb25b78b935d23dbc2f32e5ca1c0d1a13dd4d
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 11:04:32 -0300

    [feat] Added icons for services

--- Src/Presentation/src/app/pages/institucional/home.component.ts
+++ Src/Presentation/src/app/pages/institucional/home.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { ButtonModule } from 'primeng/button';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import { faCalendar, faHandshake } from "@fortawesome/free-regular-svg-icons";
+import { faRankingStar } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-institucional',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    FooterComponent,
-    ButtonModule
-  ],
+    imports: [
+        HeaderComponent,
+        FooterComponent,
+        ButtonModule,
+        FaIconComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+    faCalendar = faCalendar;
+    faHandshake = faHandshake;
+    faRankingStar = faRankingStar;
 }
