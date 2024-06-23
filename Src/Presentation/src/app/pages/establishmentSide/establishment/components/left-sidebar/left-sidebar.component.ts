6209642331c6b113b69bc93fcb78d6a1d0284374
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 13:09:37 -0300

    [feat] Imported fontawesome and fontawesome icons for left sidebar

--- Src/Presentation/src/app/pages/establishmentSide/establishment/components/left-sidebar/left-sidebar.component.ts
+++ Src/Presentation/src/app/pages/establishmentSide/establishment/components/left-sidebar/left-sidebar.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faHome, faUser, faSignOutAlt, faCalendar, faStar, faPerson, faCheck, faChair, faShop, faBell } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FontAwesomeModule,
   ],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.scss'
 })
 export class LeftSidebarComponent {
+  faHome = faHome;
+  faUser = faUser;
+  faSignOutAlt = faSignOutAlt;
+  faCalendar = faCalendar;
+  faStar = faStar;
+  faPerson = faPerson;
+  faCheck = faCheck;
+  faChair = faChair;
+  faShop = faShop;
+  faBell = faBell;
+  constructor() { }
 
 }
