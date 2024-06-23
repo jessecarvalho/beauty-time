00c56770bf3c241f477b9daf75e1e3aff08dd219
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 10:51:20 -0300

    [feat] Created interface for User Services

---
+++ Src/Application/Interfaces/IUserServices.cs
@@ -0,0 +1,13 @@
+using Application.DTOs.User;
+using Core.Entities;
+
+namespace Application.Interfaces;
+
+public interface IUserServices
+{
+    public Task<IEnumerable<UserResponseDto>?> GetAllUsersAsync();
+    public Task<UserResponseDto> GetUserByIdAsync(int id);
+    public Task<UserResponseDto> AddUserAsync(UserRequestDto user);
+    public Task<UserResponseDto> UpdateUserAsync(int id, UserRequestDto user);
+    public Task<bool> DeleteUserAsync(int id);  
+}
