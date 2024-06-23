c326a24133c2bb6dd9cc25dce57a8f1f8f5f4c3a
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 13:06:15 -0300

    [feat] Imported fontawesome and fontawesome components

--- Src/Presentation/src/app/pages/establishmentSide/establishment/pages/services/services.component.ts
+++ Src/Presentation/src/app/pages/establishmentSide/establishment/pages/services/services.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {FooterComponent} from "../../components/footer/footer.component";
+import {HeaderComponent} from "../../components/header/header.component";
+import {LeftSidebarComponent} from "../../components/left-sidebar/left-sidebar.component";
+import {SharedModule} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faPersonDigging} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+    imports: [
+        FooterComponent,
+        HeaderComponent,
+        LeftSidebarComponent,
+        SharedModule,
+        TableModule,
+        FaIconComponent
+    ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
 
+  protected readonly faPersonDigging = faPersonDigging;
 }
