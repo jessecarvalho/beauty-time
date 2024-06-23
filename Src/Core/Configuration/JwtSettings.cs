6d60a6beabc4e7dd78b5d7d7dc2c860bb642f172
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 10:53:46 -0300

    [feat] Created JWT Settings config

---
+++ Src/Core/Configuration/JwtSettings.cs
@@ -0,0 +1,9 @@
+namespace Core.Configuration;
+
+public class JwtSettings
+{
+    public string SecretKey { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public int ExpiryInMinutes { get; set; }
+}
