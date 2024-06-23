5344339aa2be7e79476aae3ab8d35e4525c2d841
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 13:08:20 -0300

    [feat] Imported minimum necessary to scratch pages

--- Src/Presentation/src/app/pages/establishmentSide/establishment/pages/calendar/calendar.component.ts
+++ Src/Presentation/src/app/pages/establishmentSide/establishment/pages/calendar/calendar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {FooterComponent} from "../../components/footer/footer.component";
+import {HeaderComponent} from "../../components/header/header.component";
+import {LeftSidebarComponent} from "../../components/left-sidebar/left-sidebar.component";
+import {SharedModule} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faPersonDigging} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+    imports: [
+        FooterComponent,
+        HeaderComponent,
+        LeftSidebarComponent,
+        SharedModule,
+        TableModule,
+        FaIconComponent
+    ],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
 
+  protected readonly faPersonDigging = faPersonDigging;
 }
--- Src/Presentation/src/app/pages/establishmentSide/establishment/pages/establishment/establishment.component.ts
+++ Src/Presentation/src/app/pages/establishmentSide/establishment/pages/establishment/establishment.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {FooterComponent} from "../../components/footer/footer.component";
+import {HeaderComponent} from "../../components/header/header.component";
+import {LeftSidebarComponent} from "../../components/left-sidebar/left-sidebar.component";
+import {faPersonDigging} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-establishmentSide',
   standalone: true,
-  imports: [],
+    imports: [
+        FaIconComponent,
+        FooterComponent,
+        HeaderComponent,
+        LeftSidebarComponent
+    ],
   templateUrl: './establishment.component.html',
   styleUrl: './establishment.component.scss'
 })
 export class EstablishmentComponent {
 
+  protected readonly faPersonDigging = faPersonDigging;
 }
