a46187e5e931f978f98456e88b2b438a53be3f31
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 10:57:09 -0300

    [feat] Imported fontawesome  and some fontawesome components

--- Src/Presentation/src/app/app.component.ts
+++ Src/Presentation/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
