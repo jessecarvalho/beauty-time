b9861119d488b18fdfd5ae643690a939d5781485
jessecarvalho <jesseelias80@gmail.com>
2024-06-23 11:03:37 -0300

    [feat] Imported routerLink

--- Src/Presentation/src/app/components/header/header.component.ts
+++ Src/Presentation/src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
