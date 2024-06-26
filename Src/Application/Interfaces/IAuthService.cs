48f72703e86b306de307f1d20c54452d621519a9
jessecarvalho <jesseelias80@gmail.com>
2024-06-26 13:01:47 -0300

    [feat] Created function to get user from request by its token

--- Src/Application/Interfaces/IAuthService.cs
+++ Src/Application/Interfaces/IAuthService.cs
@@ -1,9 +1,12 @@
 using Application.DTOs.Authentication;
 using Application.DTOs.User;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Interfaces;
 
 public interface IAuthService
 {
     Task<string> LoginAsync(LoginRequestDto loginRequest);
+    Task<User> GetUserFromRequestAsync(HttpRequest request);
 }
--- Src/Application/Services/AuthService.cs
+++ Src/Application/Services/AuthService.cs
@@ -1,10 +1,13 @@
+using System.IdentityModel.Tokens.Jwt;
 using Application.DTOs.Authentication;
 using Application.DTOs.User;
 using Application.Interfaces;
 using AutoMapper;
+using Core.Entities;
 using Infrastructure.Exceptions;
 using Infrastructure.Interfaces;
 using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Services;
 
@@ -22,6 +25,16 @@ public class AuthService : IAuthService
         _tokenService = tokenService;
     }
     
+    public async Task<User> GetUserFromRequestAsync(HttpRequest request)
+    {
+        string authHeader = request.Headers["Authorization"]!;
+        if (string.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Bearer "))
+            throw new UnauthorizedAccessException("Invalid token.");
+
+        string token = authHeader.Substring("Bearer ".Length).Trim();
+        return await GetUserFromTokenAsync(token);
+    }
+    
     public async Task<string> LoginAsync(LoginRequestDto loginRequest)
     {
         var user = await _userRepository.GetUserByEmailAsync(loginRequest.Email);
@@ -37,5 +50,17 @@ public class AuthService : IAuthService
         var userDto = _mapper.Map<UserResponseDto>(user);
         var token = _tokenService.GenerateToken(user.Id.ToString());
         return token;
+    }
+
+    public async Task<User> GetUserFromTokenAsync(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var jwtToken = handler.ReadJwtToken(token);
+        
+        var userIdClaim = jwtToken.Claims.First(claim => claim.Type == JwtRegisteredClaimNames.Sub);
+        
+        var user = await _userRepository.GetByIdAsync(int.Parse(userIdClaim.Value));
+
+        return user;
     }
 }
