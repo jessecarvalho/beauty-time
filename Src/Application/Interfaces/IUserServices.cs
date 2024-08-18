d009467fc130e6d8d36b68120dcf8a24c07421ab
jessecarvalho <jesseelias80@gmail.com>
2024-08-18 14:33:46 -0300

    [feat] Receive logged user as param

--- Src/Application/Interfaces/IUserServices.cs
+++ Src/Application/Interfaces/IUserServices.cs
@@ -8,6 +8,6 @@ public interface IUserServices
     public Task<IEnumerable<UserResponseDto>?> GetAllUsersAsync();
     public Task<UserResponseDto> GetUserByIdAsync(int id);
     public Task<UserResponseDto> AddUserAsync(UserRequestDto user);
-    public Task<UserResponseDto> UpdateUserAsync(int id, UserRequestDto user);
+    public Task<UserResponseDto> UpdateUserAsync(int id, UserRequestDto user, int loggedUser);
     public Task<bool> DeleteUserAsync(int id);  
 }
