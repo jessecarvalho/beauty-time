530320e8ebe4bf6f4d5259be2c56184085b78359
jessecarvalho <jesseelias80@gmail.com>
2024-08-18 14:40:25 -0300

    [feat] Make some fields required

--- Src/Core/Entities/Establishment.cs
+++ Src/Core/Entities/Establishment.cs
@@ -7,36 +7,41 @@ namespace Core.Entities;
 
 public record Establishment
 {
+    public Establishment(List<Service>? services)
+    {
+        Services = services;
+    }
+
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
-    
+    public int Id { get; init; }
+
     [Required]
     [MaxLength(255)]
-    public string Name { get; set; }
-    
+    public required string Name { get; set; }
+
     [MaxLength(255)]
-    public string Logo { get; set; }
-    
+    public required string Logo { get; set; }
+
     [MaxLength(255)]
-    public string Cover { get; set; }
-    
+    public required string Cover { get; set; }
+
     [Required]
     [MaxLength(255)]
-    public string Permalink { get; set; }
-    
+    public required string Permalink { get; set; } 
+
     [Required]
     [MaxLength(255)]
-    public string Address { get; set; }
+    public required string Address { get; set; }
+
+    [Required]
+    public required ActiveEnum Active { get; set; }
     
     [Required]
-    public ActiveEnum Active { get; set; }
-    
-    public List<Appointment> Appointments { get; set; }
-    
-    public List<Client> Clients { get; set; }
-    public List<Service> Services { get; set; }
-    
-    public User User { get; set; }
-    
+    public required int UserId { get; set; }
+
+    public List<Service>? Services { get; set; }
+
+    public required User User { get; init; }
+
 }
