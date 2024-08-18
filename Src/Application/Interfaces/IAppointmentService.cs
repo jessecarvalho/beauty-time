4fc738323e745eacfe6368c989b53f960a587318
jessecarvalho <jesseelias80@gmail.com>
2024-08-18 14:35:06 -0300

    [feat] Receive user id as param and add it in request

--- Src/Application/Interfaces/IAppointmentService.cs
+++ Src/Application/Interfaces/IAppointmentService.cs
@@ -6,8 +6,8 @@ namespace Application.Interfaces;
 public interface IAppointmentService
 {
     Task<IEnumerable<AppointmentResponseDto>> GetAllAsync(int userId, string cacheKey);
-    Task<IEnumerable<AppointmentResponseDto>> GetByIdAsync(int userId, int id, string cacheKey);
+    Task<IEnumerable<AppointmentResponseDto>> GetByIdAsync(int id, int userId, string cacheKey);
     Task<AppointmentResponseDto> AddAsync(int userId, AppointmentRequestDto appointmentRequestDto, string cacheKey);
     Task<AppointmentResponseDto> UpdateAsync(int userId, int id, AppointmentRequestDto appointmentRequestDto, string cacheKey);
-    Task<bool> RemoveAsync(int userId, int id);
+    Task<bool> RemoveAsync(int id, int userId);
 }
--- Src/Application/Services/AppointmentService.cs
+++ Src/Application/Services/AppointmentService.cs
@@ -13,36 +13,36 @@ public class AppointmentService : IAppointmentService
     private readonly IMapper _mapper;
     private readonly IAppointmentRepository _appointmentRepository;
     private readonly IRedisService _redisService;
+    private readonly IEstablishmentRepository _establishmentRepository;
 
-    public AppointmentService(IAppointmentRepository appointmentRepository, IMapper mapper, IRedisService redisService)
+    public AppointmentService(IAppointmentRepository appointmentRepository, IMapper mapper, IRedisService redisService, IEstablishmentRepository establishmentRepository)
     {
         _appointmentRepository = appointmentRepository;
         _mapper = mapper;
         _redisService = redisService;
+        _establishmentRepository = establishmentRepository;
     }
 
     public async Task<IEnumerable<AppointmentResponseDto>> GetAllAsync(int userId, string cacheKey)
     {
-        
         var appointmentsInCache = await _redisService.GetValueAsync(cacheKey);
-        
+    
         if (appointmentsInCache is not null)
         {
             var cachedAppointments = JsonSerializer.Deserialize<IEnumerable<AppointmentResponseDto>>(appointmentsInCache);
             if (cachedAppointments is not null)
                 return cachedAppointments;
         }
-        
+
         var appointments = await _appointmentRepository.GetAllAsync(userId);
+    
         await _redisService.SetValueAsync(cacheKey, JsonSerializer.Serialize(appointments), TimeSpan.FromMinutes(1));
-        
+    
         return _mapper.Map<IEnumerable<AppointmentResponseDto>>(appointments);
-        
     }
 
-    public async Task<IEnumerable<AppointmentResponseDto>> GetByIdAsync(int userId, int id, string cacheKey)
+    public async Task<IEnumerable<AppointmentResponseDto>> GetByIdAsync(int id, int userId, string cacheKey)
     {
-        
         var appointmentInCache = await _redisService.GetValueAsync(cacheKey);
         
         if (appointmentInCache is not null)
@@ -52,7 +52,8 @@ public class AppointmentService : IAppointmentService
                 return new List<AppointmentResponseDto> { cachedAppointment };
         }
         
-        var appointment = await _appointmentRepository.GetByIdAsync(userId, id);
+        var appointment = await _appointmentRepository.GetByIdAsync( id, userId);
+        
         await _redisService.SetValueAsync(cacheKey, JsonSerializer.Serialize(appointment), TimeSpan.FromMinutes(1));
         
         return _mapper.Map<IEnumerable<AppointmentResponseDto>>(appointment);
@@ -69,14 +70,17 @@ public class AppointmentService : IAppointmentService
             if (cachedAppointment is not null)
                 return cachedAppointment;
         }
-        
-        var appointment = await _appointmentRepository.AddAsync(userId, _mapper.Map<Appointment>(appointmentRequestDto));
+
+        var appointmentObj = _mapper.Map<Appointment>(appointmentRequestDto);
+        appointmentObj.UserId = userId;
+
+        var appointment = await _appointmentRepository.AddAsync(userId, appointmentObj);
         await _redisService.SetValueAsync(cacheKey, JsonSerializer.Serialize(appointment), TimeSpan.FromMinutes(1));
         
         return _mapper.Map<AppointmentResponseDto>(appointment);
     }
     
-    public async Task<AppointmentResponseDto> UpdateAsync(int userId, int id, AppointmentRequestDto appointmentRequestDto, string cacheKey)
+    public async Task<AppointmentResponseDto> UpdateAsync(int id, int userId, AppointmentRequestDto appointmentRequestDto, string cacheKey)
     {
         var appointmentInCache = await _redisService.GetValueAsync(cacheKey);
         
@@ -87,14 +91,17 @@ public class AppointmentService : IAppointmentService
                 return cachedAppointment;
         }
         
-        var appointment = await _appointmentRepository.UpdateAsync(userId, id, _mapper.Map<Appointment>(appointmentRequestDto));
+        var appointmentObj = _mapper.Map<Appointment>(appointmentRequestDto);
+        appointmentObj.UserId = userId;
+        
+        var appointment = await _appointmentRepository.UpdateAsync(userId, id, appointmentObj);
         await _redisService.SetValueAsync(cacheKey, JsonSerializer.Serialize(appointment), TimeSpan.FromMinutes(1));
         
         return _mapper.Map<AppointmentResponseDto>(appointment);
     }
     
-    public async Task<bool> RemoveAsync(int userId, int id)
+    public async Task<bool> RemoveAsync(int id, int userId)
     {
-        return await _appointmentRepository.RemoveAsync(userId, id);
+        return await _appointmentRepository.RemoveAsync(id, userId);
     }
 }
